3b4933eb09ee780eac196665538fee0a60a10e8b
amits4chetu <amits4@chetu.com>
2021-01-18 01:21:45 +0530

    express item updated

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const webpath=path.join(__dirname,'../public');
 
 app.use(express.static(webpath));
 
-// app.get('/',(req,res)=>{
-//     res.send("Hi Amit");
-// });
+app.get('/',(req,res)=>{
+    // res.send("Hi Amit");
+});
 app.get('/temp',(req,res)=>{
     // requests(
 
